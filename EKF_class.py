5b3e4f4278926b388428fc21bf76e356c4776636
annasulzer <144078462+annasulzer@users.noreply.github.com>
2024-06-07 11:18:51 +0200

    good tuning for mean at least

--- EKF_class.py
+++ EKF_class.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class KalmanFilter:
@@ -8,7 +9,7 @@ class KalmanFilter:
         self.del_t = del_t
         self.Q = 1 * del_t * np.eye(len(mu0))
         self.Q[:3, :3] = 0.000001 * np.eye(3)#angular velocity
-        self.R = 0.00001 * np.eye(11)
+        self.R = 0.01 * np.eye(33)
 
     def predict(self):
 
@@ -29,6 +30,7 @@ class KalmanFilter:
             STM[i:i+3, i:i+3] = [[1, -self.del_t * omega_z, self.del_t * omega_y],
                                 [self.del_t * omega_z, 1, -self.del_t * omega_x],
                                 [-self.del_t * omega_y, self.del_t * omega_x, 1]]
+                                
         self.del_t = 2*self.del_t 
                           
         
@@ -36,32 +38,45 @@ class KalmanFilter:
         
         mu_predict = self.A @ self.mu 
         sigma_predict = self.A @ self.sigma @ self.A.T + self.Q
-
+      
         return mu_predict, sigma_predict
 
     def update(self, y, mu_est, sigma_est, sat_pos):
-
+        
         valid_indices = [i for i in range(len(y)) if not np.isnan(y[i])]
 
+        valid_indices1 = np.mod(valid_indices, 11)#find all without repeat
+  
         # Initialize 
-        self.C = np.zeros((len(valid_indices), len(mu_est)))
-        g = np.zeros(len(valid_indices))
+        self.C = np.zeros((len(valid_indices1), len(mu_est)))
+        g = np.zeros(len(valid_indices1))
 
-        for idx, i in enumerate(valid_indices):
-            j = 3 + 3 * i
+        for idx, i in enumerate(valid_indices1):
+            if (idx > 21):
+                n = 2
+            elif (idx>10):
+                n = 1
+            else:
+                n = 0
+            
+            j = 3 + 3 * np.mod(i, 11)
             point_pos = mu_est[j:j+3]
             
-            dist = np.linalg.norm(point_pos - sat_pos)
-            
-            self.C[idx, j:j+3] = (point_pos - sat_pos) / dist
+            dist = np.linalg.norm(point_pos - sat_pos[n])
+
+  
+            self.C[idx, j:j+3] = (point_pos - sat_pos[n]) / dist
             g[idx] = dist
 
-        if len(valid_indices) == 0:
-            print("No valid measurements available for update.")
-            return mu_est, sigma_est
+
+        #plt.spy(self.C)
 
         valid_indices = np.array(valid_indices)
-        K = sigma_est @ self.C.T @ np.linalg.inv(self.C @ sigma_est @ self.C.T + self.R[valid_indices[:, None], valid_indices]) #eliminate all dimensions that are nan 
+  
+        R_valid = self.R[valid_indices[:, None], valid_indices]
+        #R_valid += np.eye(R_valid.shape[0]) * 1e-9
+
+        K = sigma_est @ self.C.T @ np.linalg.inv(self.C @ sigma_est @ self.C.T + R_valid) #eliminate all dimensions that are nan 
         #plt.spy() visualize sparsity structure of matrix -> check if sigma_est and C if block diagonalor sufficially close then we can do incremental update
         #information filter (not recommended)
         y_valid = y[valid_indices]
@@ -71,7 +86,7 @@ class KalmanFilter:
         self.sigma = sigma_est - K @ self.C @ sigma_est
         O = np.vstack((self.C, self.C@self.A, self.C@self.A@self.A.T))
 
-
+       
         return self.mu, self.sigma, np.linalg.matrix_rank(O)
 
 
